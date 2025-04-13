810927ca742988e3dd437a4b9e89f739005b2982
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2025-04-13 17:56:19 +0700

    feat: add shadcn that compatible with tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [
+    sveltekit(),
+    tailwindcss()
+  ],
+  server: {
+    port: 3000
+  }
 });
